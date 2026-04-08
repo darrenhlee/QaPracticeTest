c56af380865ede3aebb47e8fbfc061f0cda1217c
Darren Lee <darren.lee@outlook.com>
2026-04-08 19:45:18 -0400

    Add test for the correct labels in multiple checkbox page.

--- Tests/Checkboxes/MultipleCheckboxTests.cs
+++ Tests/Checkboxes/MultipleCheckboxTests.cs
@@ -1,5 +1,4 @@
 ﻿using QaPracticeTest.Pages.Checkboxes;
-using System.Threading.Tasks;
 
 namespace QaPracticeTest.Tests.Checkboxes
 {
@@ -16,5 +15,26 @@ namespace QaPracticeTest.Tests.Checkboxes
 
         [Test]
         public async Task NumberOfCheckboxesIsCorrect() => await Expect(CheckboxPage.Checkboxes).ToHaveCountAsync(3);
+
+        [Test]
+        public async Task LabelsOfCheckboxesAreCorrect()
+        {
+            var expectedLabels = new[]
+            {
+                "One",
+                "Two",
+                "Three"
+            };
+
+            for (var i = 0; i < expectedLabels.Length; i++)
+            {
+                var checkboxID = await CheckboxPage.Checkboxes.Nth(i).GetAttributeAsync("id");
+                if (checkboxID is not null)
+                {
+                    var label = Page.Locator($"[for={checkboxID}]");
+                    await Expect(label).ToHaveTextAsync(expectedLabels[i]);
+                }
+            }
+        }
     }
 }
