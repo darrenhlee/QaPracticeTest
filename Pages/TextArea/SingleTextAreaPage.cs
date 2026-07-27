8c0faeb282bbd810e0a1f0e36cd88ce2ce42c098
Darren Lee <darren.lee@outlook.com>
2026-07-27 15:14:58 -0400

    Add single text area tests.

---
+++ Pages/TextArea/SingleTextAreaPage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Playwright;
+
+namespace QaPracticeTest.Pages.TextArea
+{
+    public class SingleTextAreaPage(IPage page) : ResultPage(page, "https://www.qa-practice.com/elements/textarea/single")
+    {
+        public ILocator TextAreaLabel => _page.Locator("label[for='id_text_area']");
+        public ILocator TextArea => _page.GetByRole(AriaRole.Textbox, new() { NameString = "Text area*" });
+        public ILocator SubmitButton => _page.GetByRole(AriaRole.Button, new() { NameString = "Submit" });
+    }
+}
---
+++ Tests/TextArea/SingleTextAreaTests.cs
@@ -0,0 +1,33 @@
+﻿using QaPracticeTest.Pages.TextArea;
+
+namespace QaPracticeTest.Tests.TextArea
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class SingleTextAreaTests : PageTest
+    {
+        private SingleTextAreaPage TextAreaPage { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            TextAreaPage = new SingleTextAreaPage(Page);
+            await TextAreaPage.GoToAsync();
+        }
+
+        [Test]
+        public async Task FieldNameIsCorrect() => await Expect(TextAreaPage.TextAreaLabel).ToHaveTextAsync("Text area*");
+
+        [Test]
+        public async Task UserCanEnterAndSubmitAnyTextIntoThisField()
+        {
+            string text = "This is a test\r\ninput for the text area.";
+            await TextAreaPage.TextArea.FillAsync(text);
+            await TextAreaPage.SubmitButton.ClickAsync();
+            await Expect(TextAreaPage.Result).ToHaveTextAsync(text);
+        }
+
+        [Test]
+        public async Task FieldIsRequired() => await Expect(TextAreaPage.TextArea).ToHaveAttributeAsync("required", string.Empty);
+    }
+}
