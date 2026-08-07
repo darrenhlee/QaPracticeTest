3b9f8845cd33558e68605cda7e19e02a4eb8bef5
Darren Lee <darren.lee@outlook.com>
2026-08-07 17:53:32 -0400

    Add Iframe test.

---
+++ Pages/Iframes/IframePage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+namespace QaPracticeTest.Pages.Iframes
+{
+    public class IframePage : QaPracticePage
+    {
+        public IframePage(IPage page) : base(page, "/elements/iframe/iframe_page")
+        {
+        }
+
+        public ILocator Iframe => Page.Locator("iframe");
+        public IFrameLocator FrameContent => Page.FrameLocator("iframe");
+    }
+}
---
+++ Tests/Iframes/IframesTests.cs
@@ -0,0 +1,23 @@
+﻿using QaPracticeTest.Pages.Iframes;
+
+namespace QaPracticeTest.Tests.Iframes
+{
+    public class IframesTests : PageTest
+    {
+        private IframePage IframePage { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            IframePage = new IframePage(Page);
+            await IframePage.GoToAsync();
+        }
+
+        [Test]
+        public async Task IframeIsVisible()
+        {
+            await Expect(IframePage.Iframe).ToBeVisibleAsync();
+            await Expect(IframePage.FrameContent.Locator("div.page-content")).ToHaveTextAsync(new Regex(".+"));
+        }
+    }
+}
