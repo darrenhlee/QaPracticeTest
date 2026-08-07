ca5d32a3615b61ad02500a8bea2d7684530bdc85
Darren Lee <darren.lee@outlook.com>
2026-08-07 15:53:53 -0400

    Add draggable image tests.

---
+++ Pages/DragAndDrop/ImagesPage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+
+namespace QaPracticeTest.Pages.DragAndDrop
+{
+    public class ImagesPage : QaPracticePage
+    {
+        public ImagesPage(IPage page) : base(page, "/elements/dragndrop/images")
+        {
+        }
+
+        public ILocator Droppable1 => Page.Locator("id=rect-droppable1");
+        public ILocator Droppable2 => Page.Locator("id=rect-droppable2");
+        public ILocator DraggableImage => Page.Locator("css=img.rect-draggable");
+    }
+}
---
+++ Tests/DragAndDrop/ImagesTests.cs
@@ -0,0 +1,47 @@
+﻿using QaPracticeTest.Pages.DragAndDrop;
+
+namespace QaPracticeTest.Tests.DragAndDrop
+{
+    [TestFixture]
+    public class ImagesTests : PageTest
+    {
+        private ImagesPage ImagesPage { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            ImagesPage = new ImagesPage(Page);
+            await ImagesPage.GoToAsync();
+        }
+
+        [Test]
+        public async Task SmileyCanBeDraggedFromSquareToSquare()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                await DragSmileyToBottomSquareAndExpectDroppedDisplay();
+                await DragSmileyToTopSquareAndExpectDroppedDisplay();
+            }
+        }
+
+        private async Task DragSmileyToTopSquareAndExpectDroppedDisplay()
+        {
+            await ImagesPage.DraggableImage.DragToAsync(ImagesPage.Droppable2);
+            await Expect(ImagesPage.Droppable2).ToHaveTextAsync("Dropped!");
+            await Expect(ImagesPage.Droppable1.Filter(new()
+            {
+                Has = ImagesPage.Droppable1.Locator("css=*")
+            })).Not.ToBeVisibleAsync();
+        }
+
+        private async Task DragSmileyToBottomSquareAndExpectDroppedDisplay()
+        {
+            await ImagesPage.DraggableImage.DragToAsync(ImagesPage.Droppable1);
+            await Expect(ImagesPage.Droppable1).ToHaveTextAsync("Dropped!");
+            await Expect(ImagesPage.Droppable2.Filter(new()
+            {
+                Has = ImagesPage.Droppable2.Locator("css=*")
+            })).Not.ToBeVisibleAsync();
+        }
+    }
+}
